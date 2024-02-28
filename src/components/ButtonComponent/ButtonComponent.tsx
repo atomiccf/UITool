86cb8f7c0726baf86a957d01d00f2789b3ce7512
VasilyeuViacheslau <106859411+atomiccf@users.noreply.github.com>
2024-02-28 16:01:03 +0100

    first commit: -add Button.tsx

--- src/components/ButtonComponent/ButtonComponent.tsx
+++ src/components/ButtonComponent/ButtonComponent.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 import {Button} from "react-bootstrap";
 
 interface ButtonComponentProps  {
-    type: string
+    mode: string
     label: string;
     handleButton?: () => void
 }
 
-export const ButtonComponent: React.FC<ButtonComponentProps> = ({type,handleButton, label}) => {
+export const ButtonComponent: React.FC<ButtonComponentProps> = ({mode,handleButton, label}) => {
     return (
         <>
-            {type === 'light' && <Button onClick={handleButton} variant="primary">{label}</Button> }
-            {type === 'fullwidth' && <Button onClick={handleButton} variant="primary" size="lg">{label}</Button>}
+            {mode === 'light' && <Button onClick={handleButton} variant="primary">{label}</Button> }
+            {mode === 'fullwidth' && <Button onClick={handleButton} variant="primary" size="lg">{label}</Button>}
         </>)
 }
