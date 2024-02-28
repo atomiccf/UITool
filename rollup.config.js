a03e935120d67fe102bbf9a034381456e314c957
VasilyeuViacheslau <106859411+atomiccf@users.noreply.github.com>
2024-02-28 16:30:47 +0100

    first commit: -add Button.tsx

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
+import css from 'rollup-plugin-css-only';
 import dts from "rollup-plugin-dts";
 
 import packageJson from "./package.json" assert { type: "json" };
@@ -26,6 +27,7 @@ export default [
             resolve(),
             commonjs(),
             typescript({ tsconfig: "./tsconfig.json" }),
+            css({ output: 'bundle.css' }),
 
         ],
     },
