78978d34dd367826721b2e7907ecf3382b600e1d
VasilyeuViacheslau <106859411+atomiccf@users.noreply.github.com>
2024-02-29 12:43:20 +0100

    -add DropDownComponent.tsx

--- src/components/DropDownComponent/DropDownComponent.tsx
+++ src/components/DropDownComponent/DropDownComponent.tsx
@@ -7,6 +7,9 @@ export const DropDownComponent:React.FC<DropDownComponentProps> = ({dropdownList
     return (
         <Dropdown data-bs-theme="dark">
             <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
+                Links
+            </Dropdown.Toggle>
+            <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
              </Dropdown.Toggle>
             <Dropdown.Menu>
                 {dropdownList.map((item) => (<Dropdown.Item key={item} >{item}</Dropdown.Item>))}
