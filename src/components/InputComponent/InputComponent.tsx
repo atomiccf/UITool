84640cf76cd269b7361e5ab7a83bc4d1fe8d9e22
VasilyeuViacheslau <106859411+atomiccf@users.noreply.github.com>
2024-02-29 11:13:17 +0100

    first commit: -add SelectComponent.tsx

--- src/components/InputComponent/InputComponent.tsx
+++ src/components/InputComponent/InputComponent.tsx
@@ -1,39 +1,47 @@
 import React from "react";
 import Form from 'react-bootstrap/Form';
 import {InputComponentProps} from "./InputComponent.types";
-import {Button, Row} from "react-bootstrap";
+import {Button, Col, Row} from "react-bootstrap";
 
 export const InputComponent:React.FC<InputComponentProps> = ({mode, labelText}) => {
     return(
         <>
         {mode === 'login' &&
             <Form.Group as={Row} className="mb-3" controlId="login">
-            <Form.Label htmlFor={"login"}>Login</Form.Label>
-            <Form.Control type="text" placeholder="Enter login name" />
-            </Form.Group>
+            <Form.Label htmlFor={"login"} column sm="2">Login</Form.Label>
+                <Col sm="10">
+                    <Form.Control type="text" placeholder="Enter login name" />
+                </Col>
+           </Form.Group>
             }
         {mode === 'password' &&
             <Form.Group as={Row} className="mb-3" controlId="password">
-            <Form.Label htmlFor={"password"}>Password</Form.Label>
+            <Form.Label htmlFor={"password"} column sm="2">Password</Form.Label>
+                <Col sm="10">
             <Form.Control type="password" placeholder="Enter password" />
+                </Col>
             </Form.Group>
         }
         {mode === 'email' &&
             <Form.Group as={Row} className="mb-3" controlId="email">
-            <Form.Label htmlFor={"password"}>Email</Form.Label>
+            <Form.Label htmlFor={"password"} column sm="2">Email</Form.Label>
+                <Col sm="10">
             <Form.Control type="email" placeholder="Enter email" />
+                </Col>
             </Form.Group>
         }
         {mode === 'search' &&
             <Form.Group as={Row} className="mb-3" controlId="email">
-            <Form.Control type="text" placeholder="Search" className="mr-sm-2" />
+             <Form.Control type="text" placeholder="Search" className="mr-sm-2" />
             <Button variant="outline-success">Search</Button>
             </Form.Group>
         }
         {mode === 'text' &&
             <Form.Group as={Row} className="mb-3" >
-            <Form.Label htmlFor={"password"}>{labelText}</Form.Label>
+            <Form.Label htmlFor={"password"} column sm="2">{labelText}</Form.Label>
+                <Col sm="10">
             <Form.Control type="text" />
+                </Col>
             </Form.Group>
             }
         </>
