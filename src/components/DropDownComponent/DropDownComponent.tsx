7a222ffb4de4b52ab99f2c4f46bc42f42b8e0148
VasilyeuViacheslau <106859411+atomiccf@users.noreply.github.com>
2024-03-05 13:55:52 +0100

    - fix DropDownComponent.tsx

--- src/components/DropDownComponent/DropDownComponent.tsx
+++ src/components/DropDownComponent/DropDownComponent.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import {DropDownComponentProps} from "./DropDownComponent.types";
 import {Dropdown} from "react-bootstrap";
 
-export const DropDownComponent:React.FC<DropDownComponentProps> = ({dropdownList}) => {
+export const DropDownComponent:React.FC<DropDownComponentProps> = ({dropdownName,dropdownList}) => {
     return (
         <Dropdown data-bs-theme="dark">
             <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
-                Links
+                {dropdownName}
             </Dropdown.Toggle>
             <Dropdown.Menu>
                 {dropdownList.map((item) => (<Dropdown.Item key={item} >{item}</Dropdown.Item>))}
