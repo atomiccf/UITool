524a0fdde988e5a9cff806de518de7f14820957c
VasilyeuViacheslau <106859411+atomiccf@users.noreply.github.com>
2024-03-16 18:18:08 +0100

    - added date input in InputComponent.tsx

--- src/components/SelectComponent/SelectComponent.tsx
+++ src/components/SelectComponent/SelectComponent.tsx
@@ -2,10 +2,24 @@ import Form from 'react-bootstrap/Form';
 import React from "react";
 import {SelectComponentProps} from "./SelectComponent.types";
 
-export const SelectComponent:React.FC<SelectComponentProps> = ({data, handleChange}) => {
+export const SelectComponent:React.FC<SelectComponentProps> = ({data, handleChange,switchLabel, labelText}) => {
     return (
-    <Form.Select onChange={handleChange} aria-label="Default select example" className="Select">
-    {data.map((item) => (<option key={item} value={item}>{item}</option>))}
-    </Form.Select>
+    <>
+    {!switchLabel &&
+        <Form.Select onChange={handleChange} aria-label="Default select example" className="Select">
+            {data.map((item) => (<option key={item} value={item}>{item}</option>))}
+        </Form.Select>
+    }
+    {switchLabel &&
+        <>
+            <Form.Label>{labelText}</Form.Label>
+            <Form.Select onChange={handleChange} aria-label="Default select example" className="Select">
+                {data.map((item) => (<option key={item} value={item}>{item}</option>))}
+            </Form.Select>
+        </>
+
+    }
+    </>
+
     )
 }
--- src/components/SelectComponent/SelectComponent.types.ts
+++ src/components/SelectComponent/SelectComponent.types.ts
@@ -2,4 +2,6 @@ import React from "react";
 export interface SelectComponentProps {
     data:string[]
     handleChange?: (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => void
+    switchLabel:boolean
+    labelText:string
 }
