6039b21fed5242669a0fca63fda279ca02191cb6
VasilyeuViacheslau <106859411+atomiccf@users.noreply.github.com>
2024-03-22 14:28:53 +0100

    - fixed bug in DropDownCheckBox.tsx

--- src/components/DropDownCheckBox/DropDownCheckBox.tsx
+++ src/components/DropDownCheckBox/DropDownCheckBox.tsx
@@ -3,22 +3,24 @@ import {DropdownButton,Dropdown,Form} from "react-bootstrap";
 import {DropDownCheckProps} from "./DropDownCheckBox.types";
 
 export const DropDownCheckBox: React.FC<DropDownCheckProps> = ({dropdownName,dropdownList}) => {
-    const [checkedItems, setCheckedItems] = useState({});
-    const handleCheckChange = (event) => {
+    const [checkedItems, setCheckedItems] = useState<Record<string, boolean>>({});
+    const handleCheckChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setCheckedItems({...checkedItems, [event.target.name]: event.target.checked});
     };
+    // @ts-ignore
     return (
-        <DropdownButton id="dropdown-basic-button" title={dropdownName}>
+        <DropdownButton  id="dropdown-basic-button" title={dropdownName}>
             {dropdownList.map((item, index) => (
-                  <Form.Check
+                    <Form.Check
+                        className='pr-sm-4'
                         key={index}
                         type="checkbox"
                         id={`checkbox-${index}`}
                         label={item}
                         name={item}
-                        checked={!!dropdownList[item]}
-                        onClick={handleCheckChange}
-                       />
+                        checked={checkedItems[item]}
+                        onChange={handleCheckChange}
+                    />
             ))}
         </DropdownButton>
     )
