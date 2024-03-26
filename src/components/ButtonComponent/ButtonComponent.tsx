340f984f544802b5cca8af9d0c72d6f21b7d109a
VasilyeuViacheslau <106859411+atomiccf@users.noreply.github.com>
2024-03-26 10:58:22 +0100

    - added submit Button in Component.tsx

--- src/components/ButtonComponent/ButtonComponent.tsx
+++ src/components/ButtonComponent/ButtonComponent.tsx
@@ -4,12 +4,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {ButtonComponentProps} from "./ButtonComponent.types"
 
 
-export const ButtonComponent: React.FC<ButtonComponentProps> = ({mode,handleButton,children,size}) => {
+export const ButtonComponent: React.FC<ButtonComponentProps> = ({mode,handleButton,children,size,handleSubmit}) => {
     return (
         <>
             {mode === 'primary' && <Button onClick={handleButton} variant="primary" size={size}>{children}</Button> }
             {mode === 'light' && <Button onClick={handleButton} variant="light">{children}</Button>}
             {mode === 'success' && <Button variant="success">{children}</Button>}
             {mode === 'danger' && <Button onClick={handleButton} variant="danger">{children}</Button>}
+            {mode === 'submit' && <Button size={size} onClick={handleSubmit} variant="primary">{children}</Button>}
         </>)
 }
--- src/components/ButtonComponent/ButtonComponent.types.ts
+++ src/components/ButtonComponent/ButtonComponent.types.ts
@@ -2,7 +2,8 @@ import React from "react";
 
 export interface ButtonComponentProps  {
     mode?: string
-    handleButton?: () => Promise<void> | void
+    handleButton?: () =>  void
+    handleSubmit?: () => Promise<void> | void
     children?: string | React.ReactNode
     size?: "sm" | "lg" | undefined
 }
